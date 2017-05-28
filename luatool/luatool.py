fe003a586a57d9adbfd7f7870ba760c48ca8c76a
Ricardo <makiolo@gmail.com>
2017-05-28 22:15:59 +0200

    Update luatool.py
    improve exception message

--- luatool/luatool.py
+++ luatool/luatool.py
@@ -43,6 +43,8 @@ class TransportError(Exception):
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return self.message
 
 class AbstractTransport:
     def __init__(self):
